93c6a06617bb0ef1cbd9f5b5fd9b267ec13bcde0
momomo546 <108210454+momomo546@users.noreply.github.com>
2024-07-20 01:02:50 +0900

    値オブジェクト追加：Hp,Mp

---
+++ Assets/App/Data/Parameters/Hp.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Hp{
+    private readonly int _maxHp;
+    public int maxHp { get { return _maxHp; } }
+    private readonly int _currentHp;
+    public int currentHp { get { return _currentHp; } }
+
+    public Hp(int maxHpValue, int currentHpValue) {
+		
+        // 0より小さい時には例外を発生させる
+        if (currentHpValue < 0) {
+            throw new ArgumentException("Value cannot be negative");
+        }
+        else if (currentHpValue > this._maxHp) {
+            throw new ArgumentException("Value cannot over maxHp");
+        }
+        this._currentHp = currentHpValue;
+        this._maxHp = maxHpValue;
+    }
+
+    public Hp Add(int addedHp) {
+        if (this._currentHp + addedHp > this._maxHp) {
+            return new Hp(this._maxHp, this._maxHp);
+        }
+        else {
+            return new Hp(this._maxHp, this._maxHp + addedHp);
+        }
+    }
+
+    public Hp Substract(int substructedHp) {
+        if (this._currentHp - substructedHp < 0) {
+            return new Hp(this._maxHp, 0);
+        }
+        else {
+            return new Hp(this._maxHp, this._currentHp - substructedHp);
+        }
+    }
+}
---
+++ Assets/App/Data/Parameters/Mp.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Mp{
+    private readonly int _maxMp;
+    public int maxMp { get { return _maxMp; } }
+    private readonly int _currentMp;
+    public int currentMp { get { return _currentMp; } }
+
+    public Mp(int maxMpValue, int currentMpValue) {
+		
+        // 0より小さい時には例外を発生させる
+        if (currentMpValue < 0) {
+            throw new ArgumentException("Value cannot be negative");
+        }
+        else if (currentMpValue > this._maxMp) {
+            throw new ArgumentException("Value cannot over maxMp");
+        }
+        this._currentMp = currentMpValue;
+        this._maxMp = maxMpValue;
+    }
+
+    public Mp Add(int addedMp) {
+        if (this._currentMp + addedMp > this._maxMp) {
+            return new Mp(this._maxMp, this._maxMp);
+        }
+        else {
+            return new Mp(this._maxMp, this._maxMp + addedMp);
+        }
+    }
+
+    public Mp Substract(int substructedMp) {
+        if (this._currentMp - substructedMp < 0) {
+            return new Mp(this._maxMp, 0);
+        }
+        else {
+            return new Mp(this._maxMp, this._currentMp - substructedMp);
+        }
+    }
+}
